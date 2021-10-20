30c71b1b4b77a62d9ab248bac6d1311a0bc12055
CARLOS ANTONIO MACHADO JUNIOR <63817942+Clssp@users.noreply.github.com>
2021-10-19 21:26:43 -0300

    Arquivos de exercicio
    Nesta pasta temos os arquivos do exercício de C# realizado no dia 19/10/21.
    Temos um programa simples de pizzaria com algumas opções de pedido, limpar espaços e impressão via MessegeBox.

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace Pizzaria3._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSabores = new System.Windows.Forms.Label();
+            this.cboSabor = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkAzeitona = new System.Windows.Forms.CheckBox();
+            this.chkCebola = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rdbComBorda = new System.Windows.Forms.RadioButton();
+            this.rdbSemBorda = new System.Windows.Forms.RadioButton();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSabores
+            // 
+            this.lblSabores.AutoSize = true;
+            this.lblSabores.Location = new System.Drawing.Point(41, 53);
+            this.lblSabores.Name = "lblSabores";
+            this.lblSabores.Size = new System.Drawing.Size(37, 15);
+            this.lblSabores.TabIndex = 0;
+            this.lblSabores.Text = "Sabor";
+            this.lblSabores.Click += new System.EventHandler(this.lblSabores_Click);
+            // 
+            // cboSabor
+            // 
+            this.cboSabor.FormattingEnabled = true;
+            this.cboSabor.Location = new System.Drawing.Point(84, 45);
+            this.cboSabor.Name = "cboSabor";
+            this.cboSabor.Size = new System.Drawing.Size(248, 23);
+            this.cboSabor.Sorted = true;
+            this.cboSabor.TabIndex = 1;
+            this.cboSabor.SelectedIndexChanged += new System.EventHandler(this.cboSabores_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkAzeitona);
+            this.groupBox1.Controls.Add(this.chkCebola);
+            this.groupBox1.Location = new System.Drawing.Point(34, 117);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingredientes";
+            // 
+            // chkAzeitona
+            // 
+            this.chkAzeitona.AutoSize = true;
+            this.chkAzeitona.Location = new System.Drawing.Point(7, 49);
+            this.chkAzeitona.Name = "chkAzeitona";
+            this.chkAzeitona.Size = new System.Drawing.Size(72, 19);
+            this.chkAzeitona.TabIndex = 1;
+            this.chkAzeitona.Text = "Azeitona";
+            this.chkAzeitona.UseVisualStyleBackColor = true;
+            // 
+            // chkCebola
+            // 
+            this.chkCebola.AutoSize = true;
+            this.chkCebola.Location = new System.Drawing.Point(7, 23);
+            this.chkCebola.Name = "chkCebola";
+            this.chkCebola.Size = new System.Drawing.Size(63, 19);
+            this.chkCebola.TabIndex = 0;
+            this.chkCebola.Text = "Cebola";
+            this.chkCebola.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdbComBorda);
+            this.groupBox2.Controls.Add(this.rdbSemBorda);
+            this.groupBox2.Location = new System.Drawing.Point(240, 117);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Borda Recheada";
+            // 
+            // rdbComBorda
+            // 
+            this.rdbComBorda.AutoSize = true;
+            this.rdbComBorda.Location = new System.Drawing.Point(7, 48);
+            this.rdbComBorda.Name = "rdbComBorda";
+            this.rdbComBorda.Size = new System.Drawing.Size(85, 19);
+            this.rdbComBorda.TabIndex = 0;
+            this.rdbComBorda.TabStop = true;
+            this.rdbComBorda.Text = "Com Borda";
+            this.rdbComBorda.UseVisualStyleBackColor = true;
+            // 
+            // rdbSemBorda
+            // 
+            this.rdbSemBorda.AutoSize = true;
+            this.rdbSemBorda.Location = new System.Drawing.Point(7, 22);
+            this.rdbSemBorda.Name = "rdbSemBorda";
+            this.rdbSemBorda.Size = new System.Drawing.Size(82, 19);
+            this.rdbSemBorda.TabIndex = 1;
+            this.rdbSemBorda.TabStop = true;
+            this.rdbSemBorda.Text = "Sem Borda";
+            this.rdbSemBorda.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(176, 269);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(115, 35);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(176, 323);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(115, 35);
+            this.btnImprimir.TabIndex = 5;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 388);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cboSabor);
+            this.Controls.Add(this.lblSabores);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSabores;
+        private System.Windows.Forms.ComboBox cboSabor;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkAzeitona;
+        private System.Windows.Forms.CheckBox chkCebola;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdbComBorda;
+        private System.Windows.Forms.RadioButton rdbSemBorda;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnImprimir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pizzaria3._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblSabores_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboSabores_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Preenche o comboBox com os sabores das pizzas
+            cboSabor.Items.Add("Mussarela");
+            cboSabor.Items.Add("Palmito");
+            cboSabor.Items.Add("Atum");
+            cboSabor.Items.Add("Calabresa");
+
+            //Seleciona o item da lista que tem indice = 0, o primeiro da lista
+
+            cboSabor.SelectedIndex = 0;
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            // Seleciono o item da lista que tem indece 0, que vai ser o primeiro Lugar.
+            cboSabor.SelectedIndex = 0;
+            // Coloca o cursor no ComboBox
+            cboSabor.Focus();
+
+            // Deixar o checkBom sem seleção
+            chkAzeitona.Checked = false;
+            chkCebola.Checked = false;
+
+            // Marco o RadioButton sem borda Selecionado
+            rdbSemBorda.Checked = true;
+        }
+
+         private void btnImprimir_Click(object sender, EventArgs e)
+    {
+        string strSabor;
+        //variável para concatenar os textos
+        string strPedido = null;
+        //variável que recebe o texto do ComboBox
+        strSabor = cboSabor.Text;
+        //verifica se tem algum item do ComboBox selecionado
+        //por meio da propriedade Text
+        //Se o texto for Nulo ou Vazio emite mensagem ao user
+        //e coloca o foco no cursor do cboSabor
+        if (string.IsNullOrEmpty(cboSabor.Text))
+        {
+            MessageBox.Show("Selecione um sabor de pizza!", "Pizzaria", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            cboSabor.Focus();
+            return;
+        }
+            //Estrutura swith
+
+            switch (strSabor)
+            {
+                case "Mussarela":
+                    strPedido = "Mussarela";
+                    break;
+                case "Palmito":
+                    strPedido = "Palmito";
+                    break;
+                case "Atum":
+                    strPedido = "Atum";
+                    break;
+                case "Calabresa":
+                    strPedido = "Calabresa";
+                    break;
+            }
+
+            //Neste caso podemos ter 2 selecionados, ou nenhum
+            //Concatenar o texto dostrPedido com a String definida.
+            //Verificar se o checkBox esta selecionado
+
+            if (chkAzeitona.Checked)
+            {
+                //Concatenar o teto do strPedido com a String definida
+                strPedido = strPedido + " com Azeitonas";
+            }
+            if (chkCebola.Checked)
+            {
+                //Concatenar o teto do strPedido com a String definida
+                strPedido = strPedido + " com Cebola";
+            }
+
+
+            if (rdbComBorda.Checked)
+            {
+                //Concatenar o teto do strPedido com a String definida
+                strPedido = strPedido + " com Borda Recheada";
+            }
+            else {
+                strPedido = strPedido + " sem Borda";
+            }
+
+            MessageBox.Show(strPedido, "Pizzaria", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
